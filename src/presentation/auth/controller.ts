8d3a914d087f9b554882a49ae6f65ea2aeb0c9bd
Pablo Jose <pbl808hs@gmail.com>
2024-02-26 11:30:55 -0600

    try to consuming the repository directly

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
-import { RegisterUserDto } from "../../domain";
+import { AuthRepository, RegisterUserDto } from "../../domain";
 export class AuthController {
 
+    constructor(
+        private readonly authRepository: AuthRepository
+    ) {}
 
     registerUser = (req: Request, res: Response) => {
         const [error, registerUserDto] = RegisterUserDto.create(req.body);
         if (error) return res.status(400).json({error});
-        res.json(registerUserDto)
+        // res.json(registerUserDto)
+
+        this.authRepository.register(registerUserDto!)
+        .then(user => res.json(user))
+        .catch(error => res.status(500).json(error));
     }
 
     loginUser = (req: Request, res: Response) => {
