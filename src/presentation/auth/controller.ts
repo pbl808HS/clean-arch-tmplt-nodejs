42222de764cd443061d31c3448af0304abb538da
Pablo Jose <pbl808hs@gmail.com>
2024-02-28 16:52:46 -0600

    start jwt validation from middleware

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -38,7 +38,10 @@ export class AuthController {
 
     getUsers = (req: Request, res: Response) => {
         UserModel.find()
-        .then(users => res.json(users))
+        .then(users => res.json({
+            users,
+            token: req.body.token
+        }))
         .catch(() => res.status(500).json({error: 'Internal server error'}));
     }
 
