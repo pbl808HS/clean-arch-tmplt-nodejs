a24c838324c536f482bd461b29652dd1ac81ab94
Pablo Jose <pbl808hs@gmail.com>
2024-02-28 00:04:27 -0600

    DI - inject encryption methos on datasource implementations for dependency clear

--- src/infrastructure/datasource/auth.datasource.impl.ts
+++ src/infrastructure/datasource/auth.datasource.impl.ts
@@ -1,9 +1,15 @@
 import { UserModel } from "../../data/mongodb";
 import { RegisterUserDto, UserEntity, CustomError, AuthDatasource } from '../../domain';
-import {BcryptAdapter} from "../../config";
+import { BcryptAdapter } from "../../config";
 
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
 export class AuthDatasourceImpl implements AuthDatasource {
 
+    constructor(
+        private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+        private readonly comparePassword: CompareFunction = BcryptAdapter.compare
+    ) {}
     async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
         const { name, email, password } = registerUserDto;
 
@@ -16,7 +22,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
             const user = await UserModel.create({
                 name: name,
                 email: email,
-                password: BcryptAdapter.hash(password)
+                password: this.hashPassword(password)
             });
 
             await user.save();
