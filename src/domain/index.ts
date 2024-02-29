a00149fb47539d0d7ce9088f7c21f471a836f514
Pablo Jose <pbl808hs@gmail.com>
2024-02-29 13:28:48 -0600

    export types interfaces and use cases

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,11 @@
 
 export * from './datasources/auth.datasource';
 export * from './dtos/auth/register-user.dto';
+export * from './dtos/auth/login-user.dto';
 export * from './errors/custom.error';
 export * from './entinties/user.entity';
 export * from './repositories/auth.repository';
+export * from './use-cases/auth/register-user.use-case';
+export * from './use-cases/auth/login-user.use-case';
+export * from './interfaces/user-token.interface';
+export * from './types/sign-token.type';
---
+++ src/domain/types/sign-token.type.ts
@@ -0,0 +1,1 @@
+export type SignToken = (payload: Object, duration?:string) => Promise<string|null>;
